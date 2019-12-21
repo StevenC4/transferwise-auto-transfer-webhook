9da81fe76fe3c1592f6d11c85b058ee858ecdcc1
Steven Carroll <stevencarroll90@gmail.com>
2019-12-20 22:06:38 -0700

    Set up route to create a quote, create a transfer, and fund the transfer

--- app/middleware/transferWise.js
+++ app/middleware/transferWise.js
@@ -1,59 +1,42 @@
 const asyncHandler = require('express-async-handler');
 const config = require('../../config');
-const uuidv4 = require('uuid/v4');
+const transferWise = require('../lib/transferWise');
 
-const baseUrl = config.get('transferWise.api.baseUrl');
-
-module.exports.validateShouldTransferBalance = (req, res, next) => {
-    
+module.exports.validateShouldTransferBalance = (req, _res, next) => {
     next();
 };
 
 // https://api-docs.transferwise.com/#quotes-create
-module.exports.createQuote = asyncHandler(async (req, res, next) => {
-    const response = await fetch(`${baseUrl}/v1/quotes`, {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${config.get('transferWise.api.key')}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            profile: config.get('transferWise.profile.id'),
-            source: config.get('transferWise.source.currency'),
-            target: config.get('transferWise.target.currency'),
-            rateType: 'FIXED',
-            sourceAmount: req.body.amount,
-            type: 'REGULAR'
-        })
+module.exports.createQuote = asyncHandler(async (req, _res, next) => {
+    req.quote = await transferWise.quote.create({
+        profile: config.get('transferWise.profile.id'),
+        source: config.get('transferWise.source.currency'),
+        target: config.get('transferWise.target.currency'),
+        rateType: 'FIXED',
+        sourceAmount: req.body.amount,
+        type: 'REGULAR'
     });
-    req.quote = await response.json();
     next();
 });
 
 // https://api-docs.transferwise.com/#transfers-create
-module.exports.createTransfer = asyncHandler(async (req, res, next) => {
-    const response = await fetch(`${baseUrl}/`, {
-        method: 'POST',
-        headers: {
-            Authorization: `Bearer ${config.get('transferWise.api.key')}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            targetAccount: config.get('transferWise.account.target.id'),
-            quote: req.quote.id,
-            customerTransactionId: uuidv4(),
-            details: {
-                reference: 'Trnsfrws',
-                transferPurpose: 'Other',
-                sourceOfFunds: 'Other'
-            }
-        })
+module.exports.createTransfer = asyncHandler(async (req, _res, next) => {
+    const customerTransactionId = crypto.createHash('sha1').update(JSON.stringify(req.body), 'binary').digest('hex');
+    req.transfer = await transferWise.transfer.create({
+        targetAccount: config.get('transferWise.account.target.id'),
+        quote: req.quote.id,
+        customerTransactionId,
+        details: {
+            reference: 'Trnsfrws',
+            transferPurpose: 'Other',
+            sourceOfFunds: 'Other'
+        }
     });
-    const json = await response.json();
     next();
 });
 
 // https://api-docs.transferwise.com/#transfers-fund
-module.exports.fundTransfer = asyncHandler(async (req, res, next) => {
+module.exports.fundTransfer = asyncHandler(async (req, _res, next) => {
+    req.transferStatus = await transferWise.transfer.fund(config.get('transferWise.profile.id'), req.transfer.id);
     next();
 });
--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const authorizationMiddleware = require('./middleware/authorization');
+const transferWiseMiddleware = require('./middleware/transferWise');
 const validationMiddleware = require('./middleware/validation');
 
 router.post('/balance-deposit', [
     authorizationMiddleware.verifyEventSignature,
     validationMiddleware.balanceAccountEvent,
+    transferWiseMiddleware.createQuote,
+    transferWiseMiddleware.createTransfer,
+    transferWiseMiddleware.fundTransfer,
     (req, res, next) => {
         console.log(req.body);
         next();
