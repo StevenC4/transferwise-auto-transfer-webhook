ce036cbe7ae86df857c1a1e54acbe7f1a867541c
Steven Carroll <stevencarroll90@gmail.com>
2020-01-30 08:01:42 -0700

    Added the first successful test - an authorization middleware test for the first failure case

--- test/app/middleware/authorization.spec.js
+++ test/app/middleware/authorization.spec.js
@@ -1,13 +1,36 @@
-const {promisify} = require('util');
+const assert = require('assert');
 const authorizationMiddleware = require('../../../app/middleware/authorization');
+const logger = require('../../../app/lib/loggers/app');
+const {promisify} = require('util');
+const sandbox = require('sinon').createSandbox();
+const crypto = require('crypto');
 
 describe('app/middleware/authorization.js', () => {
     describe('verifyEventSignature', () => {
-        const verifyEventSignature = promisify(authorizationMiddleware);
+        const verifyEventSignature = promisify(authorizationMiddleware.verifyEventSignature);
+        let loggerErrorStub;
+
+        before(() => {
+            loggerErrorStub = sandbox.stub(logger, 'error');
+        });
+
+        afterEach(() => {
+            sandbox.resetHistory();
+        });
+
+        after(() => {
+            sandbox.restore();
+        });
 
         it('should fail if there is no X-Signature header present on the request', async () => {
-            const req = {};
-            const error = await verifyEventSignature(req, {}).catch(error => error);
+            const cryptoCreateVerifyStub = sandbox.stub(crypto, 'createVerify');
+            const req = {header: _headerName => undefined};
+            const error = await verifyEventSignature(req, {}).catch(err => err);
+            assert.notStrictEqual(error, undefined);
+            assert.strictEqual(error.message, 'No X-Signature header present');
+            sandbox.assert.notCalled(cryptoCreateVerifyStub);
+            sandbox.assert.notCalled(loggerErrorStub);
+            cryptoCreateVerifyStub.restore();
         });
     });
 });
